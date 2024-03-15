543c127d5251018db2967155b9b6b38d4303d999
jan <57322982+mvte@users.noreply.github.com>
2024-03-15 18:52:35 -0400

    use config and visualizer

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from game.game import Game, State
+from visual import Visual
 import sys
+import json
 
 def main():
-    game = Game({
-        "dim": 35,
-        "bot": "bot",
-    })
+    config = None
+    with open("config.json", "r") as f:
+        config = json.load(f)
+    game = Game(config)
+
+    visual = None
+    if len(sys.argv) > 1 and sys.argv[1] == "visualize":
+        visual = Visual(game)
+        config["concurrentSims"] = 1
 
     while True:
         game.update()
+        
+        if visual:
+            visual.update()
 
         if game.state == State.DONE:
             break
