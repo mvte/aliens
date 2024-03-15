ccdab6f4dd3984671cd5157092f4f666ef21c694
jan <57322982+mvte@users.noreply.github.com>
2024-03-15 18:54:36 -0400

    create alien object

---
+++ game/agents/alien.py
@@ -0,0 +1,19 @@
+import random
+
+class Alien:
+    pos = None
+
+
+    def __init__(self, pos): 
+        self.pos = pos
+
+
+    # returns the next step the alien is going to take (randomly chosen from the 4 possible directions)
+    def computeNextStep(self, ship):
+        validMoves = ship.getValidMoves(self.pos)
+        if len(validMoves) == 0:
+            return self.pos
+        newPos = random.choice(validMoves)
+        self.pos = newPos
+        return newPos
+
