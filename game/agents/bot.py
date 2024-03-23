e3e9bdd869d59dc9c1eef158862d7fa9bb342d2f
jan <57322982+mvte@users.noreply.github.com>
2024-03-23 11:25:15 -0400

    move bot logic to bot class

--- game/agents/bot.py
+++ game/agents/bot.py
@@ -1,7 +1,30 @@
+import math
+
 class Bot:
     pos = None
     alienDetected = False
     receivedBeep = False
+    k = None
+    a = None
+
+    # determines if an alien is within a 2k+1 x 2k+1 square of the bot
+    def isWithinSensorRange(self, alienPos):
+        k = self.k
+        botX, botY = self.pos
+        alienX, alienY = alienPos
+
+        return abs(botX - alienX) <= k and abs(botY - alienY) <= k
+
+    # determines the probability that we receive a beep given that the crewmate is in the cell (i, j)
+    def pbbBeepGivenCrewmateInCell(self, i, j):
+        a = self.a
+        x, y = self.pos
+        dist = abs(x - i) + abs(y - j)
+
+        return math.exp(-a * (dist - 1))
+    
+    # determines the probability that we receive a beep given that the crewmate is not in the cell (i, j)
 
 
+        
 
--- game/bot_factory.py
+++ game/bot_factory.py
@@ -1,8 +1,8 @@
 from game.agents.bot1 import Bot1
 
-def botFactory(which, ship):
+def botFactory(which, ship, k, pos, a):
     match which:
         case 1:
-            return Bot1(ship)
+            return Bot1(ship, k, pos, a)
         case _:
             return None
