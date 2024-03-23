bc1d6b89cb88427543524ebca33a1733a8e5cff1
jan <57322982+mvte@users.noreply.github.com>
2024-03-23 11:29:40 -0400

    show crewmate pbb map in visualizer

--- visual.py
+++ visual.py
@@ -9,26 +9,37 @@ displays the game state
 '''
 class Visual:
     def __init__(self, game):
-        self.fig, self.ax = plt.subplots()
+        self.fig, self.ax = plt.subplots(ncols = 2)
         self.game = game
 
 
     def update(self):
-        self.ax.clear()
+        self.ax[0].clear()
+        self.ax[1].clear()
         remapped = self._remap(self.game.sims[0].ship.board)
 
-        sns.heatmap(
+        sim = sns.heatmap(
             remapped, 
-            ax=self.ax, 
+            ax=self.ax[0], 
+            vmax=5,
             cbar=False, 
             square=True, 
-            cmap="viridis",
             xticklabels=False,
             yticklabels=False,
         )
+        crewPbb = sns.heatmap(
+            self.game.sims[0].bot.crewmatePbbMap,
+            ax=self.ax[1], 
+            cmap = "rocket_r",
+            cbar=False,
+            square=True, 
+            xticklabels=False,
+            yticklabels=False,
+        )
+
         self.fig.canvas.draw()
 
-        plt.pause(0.001)
+        plt.pause(0.0001)
     
 
     def _remap(self, board):
