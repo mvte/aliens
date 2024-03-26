72cd982a436939f2aa5389d1c3ac00e16ed6313f
jan <57322982+mvte@users.noreply.github.com>
2024-03-25 20:31:18 -0400

    show heatmap for alien pbb

--- visual.py
+++ visual.py
@@ -2,6 +2,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from game.ship import Node
+from game.game import State
 
 
 '''
@@ -9,13 +10,17 @@ displays the game state
 '''
 class Visual:
     def __init__(self, game):
-        self.fig, self.ax = plt.subplots(ncols = 2)
+        self.fig, self.ax = plt.subplots(ncols = 3)
         self.game = game
 
 
     def update(self):
+        if self.game.state != State.RUNNING:
+            return
+
         self.ax[0].clear()
         self.ax[1].clear()
+        self.ax[2].clear()
         remapped = self._remap(self.game.sims[0].ship.board)
 
         sim = sns.heatmap(
@@ -36,6 +41,16 @@ class Visual:
             xticklabels=False,
             yticklabels=False,
         )
+        
+        alienPbb = sns.heatmap(
+            self.game.sims[0].bot.alienPbbMap,
+            ax=self.ax[2], 
+            cmap = "rocket_r",
+            cbar=False,
+            square=True, 
+            xticklabels=False,
+            yticklabels=False,
+        )
 
         self.fig.canvas.draw()
 
@@ -46,20 +61,27 @@ class Visual:
         lookup = {
             Node.CLOSED: 1,
             Node.OPEN: np.nan,
-            Node.ALIEN: 2,
         }
 
         arr = np.array([[lookup[node] for node in row] for row in board])
         
         botPos = self.game.sims[0].bot.pos
         crewmatePos = []
+        alienPos = []
         for crewmate in self.game.sims[0].crewmates:
             crewmatePos.append(crewmate.pos)
+        for alien in self.game.sims[0].aliens:
+            alienPos.append(alien.pos)
 
+        for pos in alienPos:
+            arr[pos[0]][pos[1]] = 2
         arr[botPos[0]][botPos[1]] = 3
         for pos in crewmatePos:
             arr[pos[0]][pos[1]] = 4
 
         return arr
+
+if __name__ == "__main__":
+    print("testing")
 
     
