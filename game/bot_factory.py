657d227bc1c67b6088fb1d6a6714d7f8ca9344d0
jan <57322982+mvte@users.noreply.github.com>
2024-03-25 20:30:57 -0400

    add bot2 to factory

--- game/bot_factory.py
+++ game/bot_factory.py
@@ -1,8 +1,11 @@
 from game.agents.bot1 import Bot1
+from game.agents.bot2 import Bot2
 
 def botFactory(which, ship, k, pos, a):
     match which:
         case 1:
             return Bot1(ship, k, pos, a)
+        case 2:
+            return Bot2(ship, k, pos, a)
         case _:
             return None
