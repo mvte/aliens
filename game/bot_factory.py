c3be5018716f5d10146177c044cfd3ee0aa194e6
jan <57322982+mvte@users.noreply.github.com>
2024-03-28 01:47:31 -0400

    add bot3

--- game/bot_factory.py
+++ game/bot_factory.py
@@ -1,5 +1,6 @@
 from game.agents.bot1 import Bot1
 from game.agents.bot2 import Bot2
+from game.agents.bot3 import Bot3
 
 def botFactory(which, ship, k, pos, a):
     match which:
@@ -7,5 +8,7 @@ def botFactory(which, ship, k, pos, a):
             return Bot1(ship, k, pos, a)
         case 2:
             return Bot2(ship, k, pos, a)
+        case 3:
+            return Bot3(ship, k, pos, a)
         case _:
             return None
