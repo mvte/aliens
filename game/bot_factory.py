5863b7bceade1e171e21674fecc2e850b849ffe3
jan <57322982+mvte@users.noreply.github.com>
2024-03-28 23:17:42 -0400

    add bot 4 and 5 to factory

--- game/bot_factory.py
+++ game/bot_factory.py
@@ -1,6 +1,8 @@
 from game.agents.bot1 import Bot1
 from game.agents.bot2 import Bot2
 from game.agents.bot3 import Bot3
+from game.agents.bot4 import Bot4
+from game.agents.bot5 import Bot5
 
 def botFactory(which, ship, k, pos, a):
     match which:
@@ -10,5 +12,9 @@ def botFactory(which, ship, k, pos, a):
             return Bot2(ship, k, pos, a)
         case 3:
             return Bot3(ship, k, pos, a)
+        case 4: 
+            return Bot4(ship, k, pos, a)
+        case 5:
+            return Bot5(ship, k, pos, a)
         case _:
             return None
